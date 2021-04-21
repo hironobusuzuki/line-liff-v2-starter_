4eb0081cedbebd45cbf335e7a747b20889eb6e19
鈴木宏伸 <hirosuzu001@gmail.com>
2021-04-21 13:29:57 +0900

    My first commit

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -200,6 +200,24 @@ function registerButtonHandlers() {
             window.location.reload();
         }
     });
+
+// scanCode call
+document.getElementById('scanQrCodeButton').addEventListener('click', function() {
+    if (!liff.isInClient()) {
+        sendAlertIfNotInClient();
+    } else {
+        if (liff.scanCode) {
+            liff.scanCode().then(result => {
+                // e.g. result = { value: "Hello LIFF app!" }
+                const stringifiedResult = JSON.stringify(result);
+                document.getElementById('scanQrField').textContent = stringifiedResult;
+                toggleQrCodeReader();
+            }).catch(err => {
+                document.getElementById('scanQrField').textContent = "scanCode failed!";
+            });
+        }
+    }
+});
 }
 
 /**
